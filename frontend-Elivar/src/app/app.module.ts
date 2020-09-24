5a763fddb5bf36c132d3d1547bdec864a522c198
alarcon_osorio <alarcon_osorio@hotmail.com>
2020-09-23 20:04:25 -0500

    Rutas. Close#6

--- frontend-Elivar/src/app/app.module.ts
+++ frontend-Elivar/src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 
+import { Routes, RouterModule, Router} from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { EstudiantesComponent } from './estudiantes/estudiantes.component';
+import { FormEstudianteComponent } from './estudiantes/form-estudiante.component';
+
+const routes:Routes = [
+  {path:'', redirectTo:'/estudiantes', pathMatch:'full'},
+  {path:'estudiantes', component:EstudiantesComponent},
+  {path:'estudiantes/form', component:FormEstudianteComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    EstudiantesComponent
+    EstudiantesComponent,
+    FormEstudianteComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
